fe8ae039458994d376229478dd910179cb096f15
aefe2 <shigaev_1990@mail.ru>
2022-11-08 19:01:38 +0700

    <

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,13 @@ document.querySelector('.button2').onclick = function () {
     document.querySelector('.button2').hidden = 'true';
 };
 
+let field = document.querySelector('.field');
+let ball = document.querySelector('.ball');
+
+football = function(event) {
+    let ballCord
+    ballCord.style.left = 'px';
+    ballCord.style.top = 'px';
+}
+
+field.addEventListener('click', football)
