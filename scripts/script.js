28516aad7ee78f5c5a1b184e58b8b08021dba924
Kakayou <shigaev_1990@mail.ru>
2022-11-11 02:43:21 +0700

    asd

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,21 @@
+//
+// document.querySelector('.button1').onclick = function () {
+//     document.querySelector('#text').style.display = 'none';
+// };
+//
+// document.querySelector('.button2').onclick = function () {
+//     document.querySelector('.button2').hidden = 'true';
+// };
+//
+// let field = document.querySelector('.field');
+// let ball = document.querySelector('.ball');
+//
+// football = function(event) {
+//     let ballCord
+//     ballCord.style.left = 'px';
+//     ballCord.style.top = 'px';
+// }
+//
+// field.addEventListener('click', football)
 
-document.querySelector('.button1').onclick = function () {
-    document.querySelector('#text').style.display = 'none';
-};
 
-document.querySelector('.button2').onclick = function () {
-    document.querySelector('.button2').hidden = 'true';
-};
-
-let field = document.querySelector('.field');
-let ball = document.querySelector('.ball');
-
-football = function(event) {
-    let ballCord
-    ballCord.style.left = 'px';
-    ballCord.style.top = 'px';
-}
-
-field.addEventListener('click', football)
