003f34251b2dfb81632616974c0f4547185bf6cb
Kakayou <shigaev_1990@mail.ru>
2022-11-12 03:45:44 +0700

    10 & 11

--- scripts/script.js
+++ scripts/script.js
@@ -1,51 +1,72 @@
 //
-// document.querySelector('.button1').onclick = function () {
-//     document.querySelector('#text').style.display = 'none';
-// };
-//
-// document.querySelector('.button2').onclick = function () {
-//     document.querySelector('.button2').hidden = 'true';
-// };
-//
-// let field = document.querySelector('.field');
-// let ball = document.querySelector('.ball');
-//
-// football = function(event) {
-//     let ballCord
-//     ballCord.style.left = 'px';
-//     ballCord.style.top = 'px';
-// }
-//
-// field.addEventListener('click', football)
+document.querySelector('.button1').onclick = function () {
+    document.querySelector('#text').hidden = 'true';
+};
 
+document.querySelector('.button2').onclick = function () {
+    document.querySelector('.button2').hidden = 'true';
+};
 
-let anchor = document.querySelector('.anchor');
-let elem = document.querySelector('.elem');
-let container = document.querySelector('.container');
-let anchorCords = anchor.getBoundingClientRect();
+let field = document.querySelector('.field');
+let fieldCords = field.getBoundingClientRect();
+let ball = document.querySelector('.ball');
+let ballCords = {};
 
-function positionAt(e) {
-    //сверху
-    if(e.clientY < anchorCords.top && e.clientX > anchorCords.left && e.clientX < anchorCords.left + anchor.offsetWidth) {
-        elem.style.top = anchorCords.top - elem.offsetHeight - 5 + 'px';
-        elem.style.left = anchorCords.left - elem.offsetWidth + elem.offsetWidth / 2 + anchor.offsetWidth / 2 + 'px';
+function football(e) {
+    ball.style.transform = 'rotate(0deg)';
+    ballCords.left = e.clientX - ball.offsetWidth / 2;
+    ballCords.top = e.clientY - ball.offsetHeight / 2;
+    //left
+    if (ballCords.left < fieldCords.left + field.clientLeft) {
+        ballCords.left = fieldCords.left + field.clientLeft;
     }
-    //справа
-    else if(e.clientX > anchorCords.left + anchor.offsetWidth) {
-        elem.style.top = anchorCords.top + 'px';
-        elem.style.left = anchorCords.left + anchor.offsetWidth +  5 +'px';
+    //right
+    if (ballCords.left + ball.offsetWidth > fieldCords.left + field.clientWidth) {
+        ballCords.left = fieldCords.left + field.offsetWidth - ball.offsetWidth - field.clientLeft;
     }
-    //снизу
-    else if(e.clientY > anchorCords.top + anchor.offsetHeight) {
-        elem.style.top = anchorCords.top + anchor.offsetHeight + 5 +'px';
-        elem.style.left = anchorCords.left - elem.offsetWidth + elem.offsetWidth / 2 + anchor.offsetWidth / 2 + 'px';
+    //top
+    if (ballCords.top < fieldCords.top + field.clientTop) {
+        ballCords.top = fieldCords.top + field.clientTop;
     }
-    //слева
-    else if(e.clientX < anchorCords.left) {
-        elem.style.top = anchorCords.top + 'px';
-        elem.style.left = anchorCords.left - elem.offsetWidth - 5 + 'px';
-
+    //bottom
+    if (ballCords.top + ball.offsetHeight > fieldCords.top + field.clientHeight) {
+        ballCords.top = fieldCords.top + field.offsetHeight - ball.offsetHeight - field.clientTop;
     }
+    ball.style.left = ballCords.left + 'px';
+    ball.style.top = ballCords.top + 'px';
+    ball.style.transform = 'rotate(360deg)';
 }
 
-container.addEventListener('mousemove', positionAt);
+field.addEventListener('click', football)
+
+//10aya
+// let anchor = document.querySelector('.anchor');
+// let elem = document.querySelector('.elem');
+// let container = document.querySelector('.container');
+// let anchorCords = anchor.getBoundingClientRect();
+//
+// function positionAt(e) {
+//     //сверху
+//     if(e.clientY < anchorCords.top && e.clientX > anchorCords.left && e.clientX < anchorCords.left + anchor.offsetWidth) {
+//         elem.style.top = anchorCords.top - elem.offsetHeight - 5 + 'px';
+//         elem.style.left = anchorCords.left - elem.offsetWidth + elem.offsetWidth / 2 + anchor.offsetWidth / 2 + 'px';
+//     }
+//     //справа
+//     else if(e.clientX > anchorCords.left + anchor.offsetWidth) {
+//         elem.style.top = anchorCords.top + 'px';
+//         elem.style.left = anchorCords.left + anchor.offsetWidth +  5 +'px';
+//     }
+//     //снизу
+//     else if(e.clientY > anchorCords.top + anchor.offsetHeight) {
+//         elem.style.top = anchorCords.top + anchor.offsetHeight + 5 +'px';
+//         elem.style.left = anchorCords.left - elem.offsetWidth + elem.offsetWidth / 2 + anchor.offsetWidth / 2 + 'px';
+//     }
+//     //слева
+//     else if(e.clientX < anchorCords.left) {
+//         elem.style.top = anchorCords.top + 'px';
+//         elem.style.left = anchorCords.left - elem.offsetWidth - 5 + 'px';
+//
+//     }
+// }
+//
+// container.addEventListener('mousemove', positionAt);
