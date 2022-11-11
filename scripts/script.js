814fd797d1829b69143774fecdcb408e7624e6b5
Kakayou <shigaev_1990@mail.ru>
2022-11-12 02:18:37 +0700

    10 gotova

--- scripts/script.js
+++ scripts/script.js
@@ -19,3 +19,33 @@
 // field.addEventListener('click', football)
 
 
+let anchor = document.querySelector('.anchor');
+let elem = document.querySelector('.elem');
+let container = document.querySelector('.container');
+let anchorCords = anchor.getBoundingClientRect();
+
+function positionAt(e) {
+    //сверху
+    if(e.clientY < anchorCords.top && e.clientX > anchorCords.left && e.clientX < anchorCords.left + anchor.offsetWidth) {
+        elem.style.top = anchorCords.top - elem.offsetHeight - 5 + 'px';
+        elem.style.left = anchorCords.left - elem.offsetWidth + elem.offsetWidth / 2 + anchor.offsetWidth / 2 + 'px';
+    }
+    //справа
+    else if(e.clientX > anchorCords.left + anchor.offsetWidth) {
+        elem.style.top = anchorCords.top + 'px';
+        elem.style.left = anchorCords.left + anchor.offsetWidth +  5 +'px';
+    }
+    //снизу
+    else if(e.clientY > anchorCords.top + anchor.offsetHeight) {
+        elem.style.top = anchorCords.top + anchor.offsetHeight + 5 +'px';
+        elem.style.left = anchorCords.left - elem.offsetWidth + elem.offsetWidth / 2 + anchor.offsetWidth / 2 + 'px';
+    }
+    //слева
+    else if(e.clientX < anchorCords.left) {
+        elem.style.top = anchorCords.top + 'px';
+        elem.style.left = anchorCords.left - elem.offsetWidth - 5 + 'px';
+
+    }
+}
+
+container.addEventListener('mousemove', positionAt);
